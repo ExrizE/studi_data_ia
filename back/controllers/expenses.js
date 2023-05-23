9e921c6215411f16ddd4a2aacfe03ba75a2fe38c
Rafih Youness <rafih.youness@gmail.com>
2023-05-23 14:13:24 +0200

    expenses routes corrections

--- back/controllers/expenses.js
+++ back/controllers/expenses.js
@@ -2,60 +2,83 @@ const db = require('../db/db');
 const { Parser } = require('json2csv');
 
 const getExpensesByCategory = async (req, res) => {
-    const spCategory = req.params.sp_category;
-
+    const dateObject = req.params;
+    const date = dateObject.date;
     try {
-        const expenses = await db.any(`
-            SELECT * 
-            FROM Collecte
-            WHERE sp_category = $1
-        `, spCategory);
-
-        res.json(expenses);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal server error' });
+        const response = await db.query('SELECT client.sp_category, collecte.details FROM collecte INNER JOIN client ON collecte.client_id = client.id WHERE date(collecte.date) = $1', [date]);
+        let result = {};
+        if (response && response.length > 0) {
+        response.forEach(row => {
+            result[row.sp_category] = Object.values(row.details);
+            });
+        res.status(200).json(result);
+        } else {
+        res.status(404).json({ error: 'Aucune donnée n\'a été trouvée pour cette date.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des données.' });
     }
 };
-
 const getAverageBasketByCategory = async (req, res) => {
-    const spCategory = req.params.sp_category;
+  try {
+      const order = ['Employé', 'Ouvrier', 'Profession intermédiaire', 'Indépendant', 'Cadre', 'Retraité', 'Sans activité'];
+      const response = await db.query('SELECT client.sp_category, AVG(collecte.total_basket) AS average_basket FROM collecte INNER JOIN client ON collecte.client_id = client.id GROUP BY client.sp_category');
+      
+      let result = response.map(row => {
+          let average_basket = parseFloat(row.average_basket);
+          if (!isNaN(average_basket)) {
+              average_basket = average_basket.toFixed(2);
+          }
+          return average_basket;
+      });
 
-    try {
-        const averageBasket = await db.one(`
-            SELECT AVG(total_basket) 
-            FROM Client
-            WHERE sp_category = $1
-        `, spCategory);
-
-        res.json(averageBasket);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal server error' });
-    }
+      result.sort((a, b) => {
+          let indexA = order.indexOf(a.sp_category);
+          let indexB = order.indexOf(b.sp_category);
+          if (indexA < indexB) {
+              return -1;
+          }
+          if (indexA > indexB) {
+              return 1;
+          }
+          return 0;
+      });
+      res.status(200).json(result);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des données.' });
+  }
 };
+
+const getDates = async (req, res) => {
+  try {
+      const response = await db.query("SELECT DISTINCT TO_CHAR(date, 'YYYY-MM-DD') as date FROM collecte ORDER BY date ASC");
+
+      const result = response.map(row => row.date);
+
+      res.status(200).json(result);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des dates.' });
+  }
+};
+
 
 const exportData = async (req, res) => {
     const rowCount = req.params.row_count;
-
     try {
-        const data = await db.any(`
-            SELECT * 
-            FROM Collecte
-            LIMIT $1
-        `, rowCount);
-
-        const json2csv = new Parser();
-        const csv = json2csv.parse(data);
-
+        const response = await db.query('SELECT * FROM collecte LIMIT $1', [rowCount]);
+        const json2csvParser = new Parser();
+        const csv = await json2csvParser.parse(response);
         res.header('Content-Type', 'text/csv');
-        res.attachment('data.csv');
+        res.attachment('collecte_data.csv');
+        console.log(csv)
         return res.send(csv);
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal server error' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Une erreur est survenue lors de l\'exportation des données.' });
     }
 };
 
-module.exports = { getExpensesByCategory, getAverageBasketByCategory, exportData };
+module.exports = { getExpensesByCategory, getAverageBasketByCategory, exportData, getDates };
--- back/routes/expenses.js
+++ back/routes/expenses.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getExpensesByCategory, getAverageBasketByCategory, exportData } = require('../controllers/expenses');
+const { getExpensesByCategory, getAverageBasketByCategory, exportData, getDates } = require('../controllers/expenses');
 
-router.get('/category/:sp_category', getExpensesByCategory);
-router.get('/average/:sp_category', getAverageBasketByCategory);
+router.get('/category/:date', getExpensesByCategory);
+router.get('/average', getAverageBasketByCategory);
 router.get('/export/:row_count', exportData);
+router.get('/dates', getDates);
 
 module.exports = router;
